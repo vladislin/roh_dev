e5a52592c05bd3cf3b4198302a600afaf831cdbd
vlads <vlad.slinchuk@gmail.com>
2020-08-29 13:49:19 +0300

    'debug1'

--- rohatynska/settings.py
+++ rohatynska/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ldfy=yo0o$p_fcx!!%#*__qpm2*tke
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG'))
 
 ALLOWED_HOSTS = ['rohatynska-deploy.herokuapp.com', '127.0.0.1', 'rohatynska.com.ua']
 
