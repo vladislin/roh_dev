b6402727c9e9e96811965b05115d599394373cdc
vlads <vlad.slinchuk@gmail.com>
2020-09-02 12:25:27 +0300

    'sitemap-add'

--- rohatynska/settings.py
+++ rohatynska/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ldfy=yo0o$p_fcx!!%#*__qpm2*tkeq-b=47^c_j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['rohatynska-deploy.herokuapp.com', '127.0.0.1', 'rohatynska.com.ua']
--- rohatynska/urls.py
+++ rohatynska/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('robots.txt/', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
+    path('sitemap.xml/', TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml"),),
 
 ]
 
