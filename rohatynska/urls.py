8ca2fe7d1241096d6360058e4548542a4a2f2ea0
vlads <vlad.slinchuk@gmail.com>
2020-09-02 10:28:17 +0300

    'google-change-list'

--- rohatynska/urls.py
+++ rohatynska/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('robots.txt/', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
+
 ]
 
 if settings.DEBUG:
